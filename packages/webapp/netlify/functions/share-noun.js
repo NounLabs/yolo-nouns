c760e43e355088f79651c993ebb05a815d623411
Juan M. Hernandez <juansmail@gmail.com>
2022-08-01 12:23:43 -0500

    Twitter card fixes

--- packages/webapp/netlify/functions/share-noun.js
+++ packages/webapp/netlify/functions/share-noun.js
@@ -15,17 +15,27 @@ exports.handler = async(event, context) => {
 	      'Content-Type': 'text/html'
 	    },
 	    body: `
-		    <html>
+			<!DOCTYPE html>
+			<html lang="en">
 			    <head>
+				    <meta charset="utf-8" />
+				    <meta name="viewport" content="width=device-width, initial-scale=1" />
+				    <meta
+				      name="description"
+				      content="YOLO Nouns"
+				    />
+	    			<title>YOLO Nouns</title>
+	    			<link rel="shortcut icon" type="image/jpg" href="/static/favicon.ico"/>
+
 					<meta name="twitter:card" content="summary" />
 					<meta name="twitter:site" content="@yolonouns" />
 					<meta name="twitter:title" content="YOLO Nouns">
 					<meta name="twitter:description" content="YOLO into Nouns!">
-					<meta name="twitter:image" content="/share/noun-png?n=${nounId}&h=${hash}">
+					<meta name="twitter:image" content="https://yolonouns.wtf/share/noun-png?n=${nounId}&h=${hash}">
 
 					<meta property="og:title" content="YOLO Nouns" />
 					<meta property="og:description" content="YOLO into Nouns!" />
-					<meta property="og:image" content="/share/noun-png?n=${nounId}&h=${hash}" />
+					<meta property="og:image" content="https://yolonouns.wtf/share/noun-png?n=${nounId}&h=${hash}" />
 			    
 				 	<script>
 						window.onload = function() {
@@ -34,7 +44,9 @@ exports.handler = async(event, context) => {
 				 	</script>
 			    </head>
 			    <body>
-			    	<img src="/share/noun-png?n=${nounId}&h=${hash}" />
+    				<noscript>You need to enable JavaScript to run this app.</noscript>
+			    
+			    	<img src="https://yolonouns.wtf/share/noun-png?n=${nounId}&h=${hash}" />
 			    </body>
 		    </html>
 	    `,        
