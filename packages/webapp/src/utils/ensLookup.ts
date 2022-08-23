2d84e867aa1893f9a22f441e615e3c7f6c3c7a04
Juan M. Hernandez <juansmail@gmail.com>
2022-08-23 16:37:09 -0500

    Fix NNS lookup

--- packages/webapp/src/utils/ensLookup.ts
+++ packages/webapp/src/utils/ensLookup.ts
@@ -1,8 +1,6 @@
 import { useEthers } from '@usedapp/core';
 import { useEffect, useState } from 'react';
-
-const NNS_REGISTRY = '0x3e1970dc478991b49c4327973ea8a4862ef5a4de';
-const ENS_REGISTRY = '0x00000000000c2e074ec69a0dfb2997ba6c7d2e1e';
+import { lookupNNSOrENS } from './lookupNNSOrENS';
 
 export const useReverseENSLookUp = (address: string) => {
   const { library } = useEthers();
@@ -10,31 +8,15 @@ export const useReverseENSLookUp = (address: string) => {
 
   useEffect(() => {
     let mounted = true;
-    if (address && library) {
-      library.network.ensAddress = NNS_REGISTRY;
-
-      library
-        .lookupAddress(address)
+    if (address && library) {    	
+      //library.lookupAddress(address)
+		lookupNNSOrENS(library, address)
         .then(name => {
-          if (!name) {
-          	  library.network.ensAddress = ENS_REGISTRY;
-		      library
-		        .lookupAddress(address)
-		        .then(name2 => {
-		          if (!name2) return;
-		          if (mounted) {
-		            setEns(name2);
-		          }
-		        })
-		        .catch(error => {
-		          console.log(`error resolving reverse ens lookup: `, error);
-		        });
-          }
-
-
+          if (!name) return;
+          
           if (mounted) {
-            setEns(name!);
-          }
+            setEns(name);
+          }          	
         })
         .catch(error => {
           console.log(`error resolving reverse ens lookup: `, error);
